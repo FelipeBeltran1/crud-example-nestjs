bb899b067f71fb2f7a5be67a38996d02a34408c2
Andres Cadena <andrescadena0607@gmail.com>
2021-05-21 11:57:03 -0500

    crud-example-nestjs

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module'
+import { TypeOrmModule } from '@nestjs/typeorm'
+@Module({
+  imports: [TypeOrmModule.forRoot({
+    type: 'sqlite',
+    database: ':memory:',
+    entities: ['dist/**/*.entity{.ts,.js}'],
+    synchronize: true,
+  }), UsersModule],
+})
+export class AppModule {}
